33765bcde43450df4ed60237473722b7b86e84cc
jovan <75473116+starogix@users.noreply.github.com>
2024-11-05 21:40:53 +0800

    to create respective databases and tables

---
+++ deleteAllDatabases.sql
@@ -0,0 +1,6 @@
+-- only use if want to fast remove all databases
+
+drop database buyerreview_database;
+drop database shortlist_database;
+drop database car_database;
+drop database user_management
---
+++ shortlist_database.sql
@@ -0,0 +1,17 @@
+-- create database and tables
+Create database shortlist_database;
+USE shortlist_database;
+
+DROP TABLE IF EXISTS `shortlist`;
+CREATE TABLE IF NOT EXISTS `shortlist` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `car_id` int NOT NULL,
+  `brand` varchar(100) NOT NULL,
+  `model` varchar(100) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `car_id` (`car_id`)
+)
