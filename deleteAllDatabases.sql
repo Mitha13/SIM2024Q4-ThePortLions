994d00d681e772ac3964ddbbc6e11e31b93cef2c
jovan <75473116+starogix@users.noreply.github.com>
2024-11-11 13:45:14 +0800

    Delete deleteAllDatabases.sql

--- deleteAllDatabases.sql
+++
@@ -1,6 +0,0 @@
--- only use if want to fast remove all databases
-
-drop database buyerreview_database;
-drop database shortlist_database;
-drop database car_database;
-drop database user_management
